Quaternion ok, abs_traj_input has wrong shape

FlowNetSD.py
@@ -64,21 +64,21 @@ class FlowNetSD(nn.Module):
 
 
     def forward(self, x):
-        print("Flownet input shape:", x.shape)
+        #print("Flownet input shape:", x.shape)
         out_conv0 = self.conv0(x)
-        print("out_conv0:", out_conv0.shape)
+        #print("out_conv0:", out_conv0.shape)
         out_conv1 = self.conv1_1(self.conv1(out_conv0))
-        print("out_conv1:", out_conv1.shape)
+        #print("out_conv1:", out_conv1.shape)
         out_conv2 = self.conv2_1(self.conv2(out_conv1))
-        print("out_conv2:", out_conv2.shape)
+        #print("out_conv2:", out_conv2.shape)
         out_conv3 = self.conv3_1(self.conv3(out_conv2))
-        print("out_conv3:", out_conv3.shape)
+        #print("out_conv3:", out_conv3.shape)
         out_conv4 = self.conv4_1(self.conv4(out_conv3))
-        print("out_conv4:", out_conv4.shape)
+        #print("out_conv4:", out_conv4.shape)
         out_conv5 = self.conv5_1(self.conv5(out_conv4))
-        print("out_conv5:", out_conv5.shape)
+        #print("out_conv5:", out_conv5.shape)
         out_conv6 = self.conv6_1(self.conv6(out_conv5))
-        print("out_conv6:", out_conv6.shape)
+        #print("out_conv6:", out_conv6.shape)
        
         return out_conv6
        

main.py
@@ -20,6 +20,7 @@ import numpy as np
 
 import csv
 import time
+from tqdm import tqdm
 
 
 
@@ -39,7 +40,6 @@ class MyDataset:
         print("Found {} images from the images folder.\n".format(len(self.data_files)))
         
         ## relative camera pose
-        #ORIGINAL: self.trajectory_relative = self.read_R6TrajFile('/vicon0/sampled_relative_R6.csv')
         self.trajectory_relative = self.read_R6TrajFile('/vicon0/sampled_relative_R6.csv')
         
         ## abosolute camera pose (global)
@@ -188,37 +188,36 @@ class Vinet(nn.Module):
         
         ## Input1: Feed image pairs to FlownetC
         c_in = image.view(batch_size, timesteps * C, H, W)
-        print(c_in.shape)
         c_out = self.flownet_sd(c_in)
-        print('\nFlownet output shape:', c_out.shape)
+        #print('\nFlownet output shape:', c_out.shape)
         #r_in = c_out.view(batch_size, timesteps, -1)
         r_in = c_out.view(batch_size, 1, -1)
-        print('Flownet output after flattening:', r_in.shape)
+        #print('Flownet output after flattening:', r_in.shape)
         
         ## Input2: Feed IMU records to small LSTM
         imu_out, (imu_n, imu_c) = self.rnnIMU(imu)
         imu_out = imu_out[:, -1, :]
         #print('imu_out', imu_out.shape)
         imu_out = imu_out.unsqueeze(1)
-        print('\nIMU small LSTM output shame:', imu_out.shape)
+        #print('\nIMU small LSTM output shame:', imu_out.shape)
         
         ## Combine the output of Flownet and IMU LSTM and xyzQ
         cat_out = torch.cat((r_in, imu_out), 2)#1 1 49158
         cat_out = torch.cat((cat_out, xyzQ), 2)#1 1 49165
-        print('\nShape after Flownet, IMU LSTM and xyzQ concatenation:', cat_out.shape)
+        #print('\nShape after Flownet, IMU LSTM and xyzQ concatenation:', cat_out.shape)
         
         ## Run main LSTM and flatten output
         r_out, (h_n, h_c) = self.rnn(cat_out)
         l_out1 = self.linear1(r_out[:,-1,:])
         l_out2 = self.linear2(l_out1)
         #l_out3 = self.linear3(l_out2)
-        print('\nFinal output of Deep learning network:', l_out2.shape)
+        #print('\nFinal output of Deep learning network:', l_out2.shape)
 
         return l_out2
     
     
 def train():
-    epoch = 5
+    epoch = 1
     batch = 1
     model = Vinet()
     optimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
@@ -242,7 +241,7 @@ def train():
     abs_traj = None
     
     with tools.TimerBlock("Start training") as block:
-        for k in range(epoch):
+        for k in tqdm(range(epoch)):
             for i in range(start, end):#len(mydataset)-1):
                 data, data_imu, target_f2f, target_global = mydataset.load_img_bat(i, batch)
                 data, data_imu, target_f2f, target_global = \
@@ -251,42 +250,29 @@ def train():
                 optimizer.zero_grad()
                 
                 if i == start:
-                    print("\nstart:", start)
                     ## load first SE3 pose xyzQuaternion
                     abs_traj = mydataset.getTrajectoryAbs(start)
-                    print("\nabs_traj:", abs_traj)
                     abs_traj_input = np.expand_dims(abs_traj, axis=0)
-                    print("\nabs_traj:", abs_traj_input)
                     abs_traj_input = np.expand_dims(abs_traj_input, axis=0)
-                    print("\nabs_traj:", abs_traj_input)
-                    abs_traj_input = Variable(torch.from_numpy(abs_traj_input).type(torch.FloatTensor).cuda()) 
-                    print("\nabs_traj:", abs_traj_input)
+                    abs_traj_input = Variable(torch.from_numpy(abs_traj_input).type(torch.FloatTensor).cuda())
 
                 ## Forward
-                print("\nStart forward pass")
                 output = model(data, data_imu, abs_traj_input)
                 
                 ## Accumulate pose
-                print("\nAccumulate pose")
                 numarr = output.data.cpu().numpy()
-                print(1)
-                print("\nabs_traj:", abs_traj)
-                print("\nnumarr:", numarr)
                 abs_traj = se3qua.accu(abs_traj, numarr)
-                print(2)
                 abs_traj_input = np.expand_dims(abs_traj, axis=0)
-                print(3)
                 abs_traj_input = np.expand_dims(abs_traj_input, axis=0)
-                print(4)
                 abs_traj_input = Variable(torch.from_numpy(abs_traj_input).type(torch.FloatTensor).cuda()) 
-                print(5)
                 
                 ## (F2F loss) + (Global pose loss)
                 ## Global pose: Full concatenated pose relative to the start of the sequence
-                print("\nCalculate loss")
+                print("\nModel output shape:", abs_traj_input.shape)
+                print("Target shape:", target_global.shape)
                 loss = criterion(output, target_f2f) + criterion(abs_traj_input, target_global)
+                # ABS TRAJ INPUT ON VAARA SHAPE!! PITAISI OLLA [1,7]
 
-                print("\nDo back propagation and optimize step")
                 loss.backward()
                 optimizer.step()
 

scripts/convertSampledTrajToR6.py
@@ -24,6 +24,7 @@ import sys
 import errno
 from subprocess import call
 import csv
+from tqdm import tqdm
 
 import decimal
 
@@ -32,6 +33,7 @@ sys.path.append('../')
 print(sys.path)
 from sophus.se3 import Se3
 from sophus.so3 import So3
+from sophus.quaternion import Quaternion as Qua
 
 
 import quaternion
@@ -78,25 +80,15 @@ def xyzQuaternion2se3_(arr):
     x,y,z,ww,wx,wy,wz = arr[0], arr[1], arr[2], arr[3], arr[4], arr[5], arr[6]
     trans = Matrix([x,y,z])
     ww, wx, wy, wz = normalize(ww, wx, wy, wz)
-        
+    
     q_real = ww
     q_img = Matrix([wx, wy, wz])
 
-    print()
-    print("q_real:", q_real)
-    print("q_img:", q_img)
-    print()
-
-    q = Quaternion(q_real,q_img) # TAMA AIHEITTAA ERRORIN, MIKSI???? 
-    # VAATII 4 INPUTTIA, MITA NE OVAT???
-
-    print("\n IF YOU SEE THIS PRINT, THEN PROBLEM IS SOLVED!!\n")
-
+    q = Qua(q_real,q_img) 
     R = So3(q)
-    
     RT = Se3(R, trans)
-    #print(RT.log())
-    numpy_vec = np.array(RT.log()).astype(float)  # SE3 to se3
+
+    numpy_vec = np.array(RT.log()).astype(float)  # SE3 to se3 KAATUU TAHAN!!!!
     
     return np.concatenate(numpy_vec)
 
@@ -119,8 +111,7 @@ def _get_filenames_and_classes(dataset_dir):
     
     # Calculate relative pose
     trajectory_relative = []
-    for i in range(len(trajectory_abs)-1):
-        print(i)
+    for i in tqdm(range(len(trajectory_abs)-1)):
         #timestamp [ns],p_RS_R_x [m],p_RS_R_y [m],p_RS_R_z [m],q_RS_w [],q_RS_x [],q_RS_y [],q_RS_z []
         timestamp = trajectory_abs[i+1][0]
         X, Y, Z = np.array(trajectory_abs[i+1][1:4]).astype(float) - np.array(trajectory_abs[i][1:4]).astype(float)
@@ -146,7 +137,6 @@ def _get_filenames_and_classes(dataset_dir):
     
     file_path = dataset_dir + '/vicon0/sampled_relative_R6.csv'
     print("Write to file:", file_path)
-    print("Number of rows:", len(trajectory_relative))
     with open(file_path, 'w+') as f:
         tmpStr = ",".join(trajectory_abs[0])
         f.write(tmpStr + '\n')        
@@ -159,7 +149,8 @@ def _get_filenames_and_classes(dataset_dir):
             r5 = float_to_str(trajectory_relative[i][4])
             r6 = float_to_str(trajectory_relative[i][5])
             tmpStr = str(trajectory_relative[i][0]) + ',' + r1 + ',' + r2 + ',' + r3 + ',' + r4 + ',' + r5 + ',' + r6
-            f.write(tmpStr + '\n')        
+            f.write(tmpStr + '\n')
+    print("Number of rows written:", len(trajectory_relative))     
     f.close()
     return
                 
@@ -171,19 +162,23 @@ def main():
     #_get_filenames_and_classes('/notebooks/EuRoC_modify/V2_01_easy')
     #_get_filenames_and_classes('/media/rvl/hddData1/dockerData/euroc/V2_02_medium')
     #_get_filenames_and_classes('/media/rvl/hddData1/dockerData/euroc/V2_03_difficult')
-    print(1)
+    print("\nStart process V1_01_easy")
     _get_filenames_and_classes('../../data/V1_01_easy/mav0')
-    print(2)
+
+    print("\nStart process V1_02_medium")
     _get_filenames_and_classes('../../data/V1_02_medium/mav0')
-    print(3)
+
+    print("\nStart process V1_03_difficult")
     _get_filenames_and_classes('../../data/V1_03_difficult/mav0')
-    print(4)
+
+    print("\nStart process V2_01_easy")
     _get_filenames_and_classes('../../data/V2_01_easy/mav0')
-    print(5)
+
+    print("\nStart process V2_02_medium")
     _get_filenames_and_classes('../../data/V2_02_medium/mav0')
-    print(6)
+
+    print("\nStart process V2_03_difficult")
     _get_filenames_and_classes('../../data/V2_03_difficult/mav0')
-    print(7)
 
 if __name__ == "__main__":
     main()

utils/se3qua.py
@@ -9,6 +9,8 @@ sys.path
 
 from sophus.se3 import Se3
 from sophus.so3 import So3
+from sophus.quaternion import Quaternion as SophusQuaternion
+
 #from sophus import se3 as Se3
 import numpy as np
 from sympy import *
@@ -48,9 +50,8 @@ def xyzQuaternion2se3_(arr):
         
     q_real = ww
     q_img = Matrix([wx, wy, wz])
-    print("\nq_real:", q_real)
-    print("\nq_img:", q_img)
-    q = Quaternion(q_real,q_img) # KAATUU TAHAN!!!!!
+
+    q = SophusQuaternion(q_real,q_img)
     R = So3(q) # So3
     
     RT = Se3(R, trans) #Se3
@@ -90,28 +91,16 @@ def accu(lastxyzQuaternion, new_se3r6):
     new_se3r6: np.array([se3R^6])
     """
     new_se3r6 = Matrix(np.transpose(new_se3r6)) #numpy array to sympy matrix
-    print("\nlastxyzQuaternion:", lastxyzQuaternion)
-    #print(type(new_se3r6))
-    #M_SE3 = Se3.exp(new_se3r6)
     M_SE3 = Se3.exp(new_se3r6)
-    print("\nM_SE3:", M_SE3)
-    #print(type(M_SE3.dtype))
     M_SE3 = M_SE3.matrix()
-    print("\nM_SE3:", M_SE3)
 
-    print("Accu2")
-    print("lastxyzQuaternion:", lastxyzQuaternion)
-    last = xyzQ2se3(lastxyzQuaternion) ## KAATUU TASSA RIVISSA!
-
-    print("Accu3")
+    last = xyzQ2se3(lastxyzQuaternion)
     last = Matrix(np.transpose(last))
-    print("Accu4")
+
     M_SE3_last = Se3.exp(last)
-    print("Accu5")
     M_SE3_last = M_SE3_last.matrix()
-    print("Accu6")
     accu = M_SE3_last * M_SE3
-    print("Accu7")
+    
     xyzq = SE3toXYZQuaternion(accu)
     
     return xyzq