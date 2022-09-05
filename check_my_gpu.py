0ab8e2195b0002f9b441bc63b5d76ef6c616ed2c
Mikko Saukkoriipi <saukkoriipi.mikko@gmail.com>
2022-09-05 15:47:05 +0300

    Add check harware script

---
+++ check_my_gpu.py
@@ -0,0 +1,29 @@
+import torch
+import os
+
+def check_my_gpu():
+    """ Check if system has GPU available and print possible GPU details."""
+    gpu_available = torch.cuda.is_available()
+    if gpu_available:
+        print("\nGPU found!")
+        number_of_gpus = torch.cuda.device_count()
+        print("Number of GPUs: {}".format(torch.cuda.device_count()))
+        print("GPU types:")
+        for i in range(number_of_gpus):
+            print("GPU {} type: {}".format(i, torch.cuda.get_device_name(i)))
+    else:
+        print("No GPUs available!")
+
+
+def check_my_cpu():
+    """ Check number of CPUs in partition and how many you are booking."""
+    print("\nTotal number of CPU cores found: {}".format(os.cpu_count()))
+    print("You are booking {} CPU cores.".format(len(os.sched_getaffinity(0))))
+
+
+def main():
+    check_my_cpu()
+    check_my_gpu()
+
+if __name__ == "__main__":
+    main()
